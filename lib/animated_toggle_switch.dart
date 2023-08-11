50db0bc18a9aae9476760a87fef6179e9b4b9a3c
MaddinMade <martin@splashbyte.dev>
2023-08-11 10:41:30 +0200

    fixes analyzing issue

--- lib/animated_toggle_switch.dart
+++ lib/animated_toggle_switch.dart
@@ -7,7 +7,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 part 'src/properties.dart';
 part 'src/style.dart';
